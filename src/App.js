de968c9d4315d0b97dffebdc3e4f4a29a1516c76
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-05-03 16:55:14 +0545

    Make Table Dynamically Updateable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import MyTable from './MyTable';
+import MyContext from './utils/MyContext';
+import useData from './useData';
 
 function App() {
+  const { empInfo, dispatch, CRUDData } = useData();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MyContext.Provider value={{ empInfo, dispatch, CRUDData }}>
+        <MyTable />
+      </MyContext.Provider>
     </div>
   );
 }
---
+++ src/MyTable.js
@@ -0,0 +1,110 @@
+import React, { useContext, useState, useRef } from 'react';
+import MyContext from './utils/MyContext';
+import './style/table.css';
+import AddTable from './AddTable';
+
+function MyTable() {
+  const myRef = useRef();
+  const { empInfo, CRUDData } = useContext(MyContext);
+  const [inEditMode, setInEditMode] = useState({
+    data: null,
+    status: false,
+    id: null,
+    rowId: null,
+    columnId: null,
+    value: null
+  });
+
+  const onDataClick = (id, rowId, columnId, value, data) => {
+    setInEditMode(prev => {
+      return {
+        id,
+        data,
+        status: true,
+        rowId,
+        columnId,
+        value
+      };
+    });
+  };
+
+  const checkIfEditable = (id, rowId, columnId) => {
+    if (!inEditMode.status) {
+      return false;
+    }
+    if (id === inEditMode.id && rowId === inEditMode.rowId && inEditMode.columnId === columnId) {
+      return true;
+    }
+    return false;
+  };
+
+  const onEditableChange = e => {
+    e.preventDefault();
+    setInEditMode(prevState => {
+      return {
+        ...prevState,
+        value: e.target.value
+      };
+    });
+  };
+
+  const onUpdateSubmit = e => {
+    if (e.key === 'Enter') {
+      let updatedData = [...inEditMode.data];
+      updatedData[inEditMode.columnId] = inEditMode.value;
+      CRUDData('UPDATE', { id: inEditMode.id, updatedData });
+      setInEditMode({
+        data: null,
+        status: false,
+        id: null,
+        rowId: null,
+        columnId: null,
+        value: null
+      });
+    }
+  };
+
+  return (
+    <div>
+      <p>This is Table</p>
+      <AddTable />
+      <table>
+        <thead>
+          <tr>
+            <th>S.N</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {empInfo.data.map((elem, index) => {
+            return (
+              <tr key={elem[0]}>
+                {elem.map((e, i) => {
+                  return checkIfEditable(elem[0], index, i) ? (
+                    <td key={i}>
+                      <input
+                        value={inEditMode.value}
+                        onChange={onEditableChange}
+                        type={i > 2 ? 'number' : 'text'}
+                        onKeyPress={onUpdateSubmit}
+                      />
+                    </td>
+                  ) : (
+                    <td key={i} onClick={() => onDataClick(elem[0], index, i, e, elem)}>
+                      {e}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MyTable;
